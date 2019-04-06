984829ce7cb19bd70bb1ff329931f8d69553f778
Alexander Diendorfer+ <20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.comgit config --global user.email 20122072@htlneufelden.onmicrosoft.com>
2019-04-06 12:11:37 +0200

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserContext myContext = UserContext.Instance;
+            Console.WriteLine(myContext == UserContext.Instance);
+        }
+    }
+}
---
+++ Singleton/UserContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton {
+    class UserContext {
+
+        static UserContext instance;
+
+        public int UserId { get; private set; }
+        public string Username { get; private set; }
+
+        private UserContext() {
+            
+        }
+
+        public void Load() {
+            // TODO: Load the date from somewhere
+        }
+
+
+
+        public static UserContext Instance {
+            get {
+                if (instance == null)
+                    instance = new UserContext();
+                return instance;
+            }
+        }
+    }
+}
